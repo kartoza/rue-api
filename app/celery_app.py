07aadc5dd8d6d7c61f11016e2e907a93ddc99bc1
Irwan Fathurrahman <meomancer@gmail.com>
2025-11-19 11:24:27 +0700

    Fix celery config

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,13 +1,13 @@
-from celery import Celery
 from app.core.config import settings
+from celery import Celery
+
+REDIS_URL = f"redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:6379"
 
 celery = Celery(
     "worker",
-    broker=f"{settings.REDIS_HOST}://{settings.REDIS_PASSWORD}:6379/0",
-    backend=f"{settings.REDIS_HOST}://{settings.REDIS_PASSWORD}:6379/1",
-    include=[
-        "app.tasks.generate_rue",
-    ],
+    broker=f"{REDIS_URL}/0",
+    backend=f"{REDIS_URL}/1",
+    include=["app.tasks.generate_rue"],
 )
 
 celery.autodiscover_tasks(["app.tasks"])
